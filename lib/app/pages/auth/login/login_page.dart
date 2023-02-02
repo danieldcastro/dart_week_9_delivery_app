c79d5c6bbd4c885575963caf508cd16e86eb7917
Daniel Marciano <daniel.castro09@hotmail.com>
2023-02-02 20:21:30 -0300

    ✨ Feature: Created LoginPage Layout

--- lib/app/pages/auth/login/login_page.dart
+++ lib/app/pages/auth/login/login_page.dart
@@ -1,15 +1,88 @@
 import 'package:flutter/material.dart';
 
+import '../../../core/ui/helpers/size_extensions.dart';
+import '../../../core/ui/styles/text_styles.dart';
+import '../../../core/ui/widgets/delivery_app_bar.dart';
+import '../../../core/ui/widgets/delivery_button.dart';
+
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
+      appBar: DeliveryAppBar(
+        elevation: 0,
       ),
-      body: Container(),
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Login',
+                      style: context.textStyles.textTitle,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'E-mail'),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'Senha'),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Center(
+                      child: DeliveryButton(
+                        label: 'ENTRAR',
+                        width: context.screenWidth,
+                        onPressed: () {},
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Não possui uma conta?',
+                      style: context.textStyles.textBold,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Cadastre-se',
+                        style: context.textStyles.textBold
+                            .copyWith(color: Colors.blue),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
