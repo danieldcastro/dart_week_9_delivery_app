0ee820ce921c9fcc63735796322ba8adc52538d2
Daniel Marciano <daniel.castro09@hotmail.com>
2023-02-02 15:02:24 -0300

    🔨 Model: Added toString to ProductModel

--- lib/app/models/product_model.dart
+++ lib/app/models/product_model.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
 class ProductModel {
@@ -38,4 +39,9 @@ class ProductModel {
 
   factory ProductModel.fromJson(String source) =>
       ProductModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'ProductModel(id: $id, name: $name, description: $description, price: $price, image: $image)';
+  }
 }
