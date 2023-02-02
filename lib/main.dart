1ba02086254c932fdf84eb05885cba125f290f50
Daniel Marciano <daniel.castro09@hotmail.com>
2023-02-01 22:29:44 -0300

    ✨ Feature: Created products repository and model

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'app/core/ui/config/env/env.dart';
+import 'app/core/config/env/env.dart';
 import 'app/dw9_delivery_app.dart';
 
 void main() async {
